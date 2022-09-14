10413c1129f67ed137f0671b196abbd246fd9dba
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-09-14 10:16:52 -0400

    rota de get/post produtos implementada sem middlewares

--- src/routes/auth.Route.js
+++ src/routes/auth.Route.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
 import {signIn, signUp} from '../controllers/auth.Controller.js'
+import { addProduct, getProduct } from '../controllers/product.Controller.js'
 
 const router = express.Router()
 
 router.post('/signIn', signIn)
 router.post('/signUp', signUp)
+router.post('/adicionarProdutos', addProduct);
+router.get('/produtos', getProduct);
 
 export default router
