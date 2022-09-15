c001570d10895005289b76965e479ec773a63f19
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-09-15 09:35:06 -0400

    tudo pronto com a rota carrinho

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,10 +1,12 @@
 import { MONGO_CART } from "../database/dataService.js"; 
 
 async function addToCart(req, res){
-    const {title, description, newValue } = req.body;
-    const {userId} = res.locals.user; 
+    const {title, description, newValue} = req.body;
+    const {userId} = res.locals.user;
+
     try {
-        const product = await MONGO_CART({insert:{title, description, newValue, userId}});
+        const product = await MONGO_CART({insert:{title, description, newValue, userId: userId}});
+        console.log(userId)
     console.log(product);
     res.sendStatus(201);
     } catch (error) {
@@ -15,11 +17,12 @@ async function addToCart(req, res){
 
 async function getCart(req, res){
     const {userId} = res.locals.user; 
-
+    /* const userId = "abcde"; */
     try {
         const product = await MONGO_CART({find:{userId}});
+
     console.log(product);
-    res.sendStatus(201);
+    res.status(200).send(product);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
